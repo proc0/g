27e6096ce51108d51c0eebf3b91305868aeac319
proc0 <alanvx@gmail.com>
2017-06-17 03:19:06 -0700

    formatting skull

--- src/commands/index.sh
+++ src/commands/index.sh
@@ -219,7 +219,7 @@ cmd_request(){
     local remote_target=`kvget target`
     #TODO: get name of remote repo
     local remote_repo_name=`get_remote_repo_name`
-    local url="https://git.autodesk.com/$remote_repo_name/ui/compare/$remote_target...$repo:$branch?expand=1"
+    local url="https://git.autodesk.com/$remote_repo_name/ui/compare/$remote_target...$remote_repo_name:$branch?expand=1"
     echo "Opening $url..."
     #TODO: outsource this to config to open with w/e
     open -a "/Applications/Google Chrome.app" "$url" || ret=$?
