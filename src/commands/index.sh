50f05708a16f1391474d2e017b3b5959f831b836
proc0 <alanvx@gmail.com>
2017-06-16 23:39:25 -0700

    fixing

--- src/commands/index.sh
+++ src/commands/index.sh
@@ -81,11 +81,15 @@ cmd_checkin(){
     local ret=0
 
     local msg=`kvget comment`
+    local status_code=`get_status_code`
     #no comment value
     [ -z "$msg" ] && ret=14
 
-    if [[ "`get_status_code`" == 'SYNCED' ]]; then
+    if [[ "$status_code" == 'SYNCED' ]]; then
         echo "`const TXT UP_TO_DATE`"
+    elif [[ "$status_code" == 'AHEAD' ]]; then
+        git push "`get_current_repo`" "`get_current_branch`"
+        cmd_stats        
     elif [ -n "$msg" ]; then
         git add -A .
         git commit -m "$msg" || ret=$?
