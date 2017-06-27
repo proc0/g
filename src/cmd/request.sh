2e453f28e7bc04c86e02f231999316654adc26b1
vincena <alan.vincenzi@autodesk.com>
2017-06-27 13:36:34 -0700

    fixing request url

--- src/cmd/request.sh
+++ src/cmd/request.sh
@@ -1,11 +1,10 @@
 cmd_request(){
-    set -x
     local ret=0
-    local repo=`get_current_repo`
-    local branch=`get_current_branch`
-    local remote_target=`kvget target`
     local message=`kvget comment`
+    local remote_target=`kvget target`
     local user=`get_username`
+    local branch=`get_current_branch`
+    local repo=`get_remote_repo_name $(get_current_repo)`
 
     local msg_title='' msg_body=''
 
@@ -20,10 +19,11 @@ cmd_request(){
     ([ -z "$remote_target" -o -z "$msg_title" -o -z "$msg_body" ]) \
     && return 14 #no argval
 
-    # echo "title:$msg_title, \
-    #       body:$msg_body, \
-    #       head:$user:portal-core/$branch, \
-    #       base:portal-core/$remote_target"
+    # echo "title:$msg_title 
+    #       body:$msg_body
+    #       head:$branch
+    #       base:$repo
+    #       url:$repo/pulls"
 
     #github API call
     response=`curl -v \
@@ -31,7 +31,7 @@ cmd_request(){
     -H "Content-Type: application/json" \
     -H "Authorization: token ee693db1978cc7792f1881b3433dde25fa384f25" \
     -X POST -d "{\"title\":\"$msg_title\",\"head\":\"$branch\",\"base\":\"$remote_target\",\"body\":\"$msg_body\"}" \
-    "https://git.autodesk.com/api/v3/repos/portal-core/ui/pulls"`
+    "https://git.autodesk.com/api/v3/repos/$repo/pulls"`
 
     _ret=$?
     [ $_ret -gt 0 ] && ret=$_ret
@@ -49,5 +49,9 @@ cmd_request(){
 }
 
 get_remote_repo_name(){
+    local repo_url=`git config --get remote.$1.url`
+    local repo_name=`dirname $repo_url | sed -e 's/.*\/\(.*\)/\1/'`
+    local repo_main=`echo ${repo_url##*/} | sed -e 's/\(.*\).git/\1/'`
 
+    echo "$repo_name/$repo_main"
 }
