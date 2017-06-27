a4845245cb51451c19358bc613cac58b23189399
vincena <alan.vincenzi@autodesk.com>
2017-06-27 14:38:30 -0700

    fixed pull request url

--- src/cmd/request.sh
+++ src/cmd/request.sh
@@ -1,47 +1,51 @@
 cmd_request(){
     local ret=0
-    local message=`kvget comment`
-    local remote_target=`kvget target`
+    local msg=`kvget comment`
+    local base=`kvget target`
     local user=`get_username`
+    local repo=`get_remote_repo_name`
     local branch=`get_current_branch`
-    local repo=`get_remote_repo_name $(get_current_repo)`
-
+    local domain=`get_remote_base_url`
+    local post_url="$domain/api/v3/repos/$repo/pulls"
+    #break up message into title and body
     local msg_title='' msg_body=''
+    [[ "$msg" =~ \{body\} ]] \
+    && msg_title="${msg%\{body\}*}" \
+    && msg_body="${msg#*\{body\}}" \
+    #check arguments
+    ([ -z "$branch" -o \
+       -z "$base" -o \
+       -z "$msg_title" -o \
+       -z "$msg_body" ]) \
+    && [[ $post_url =~ https:\/\/ ]] \
+    && [[ $post_url =~ [^repos\/\/] ]] \
+    && return 14 #bad arguments
 
-    [[ "$message" =~ \n ]] \
-    && msg_title="${message%\n*}" \
-    && msg_body="${message#*\n}"
+    #payload example
+    echo "Raising pull request...
+    title: $msg_title 
+    body: $msg_body
+    head: $branch
+    base: $base
+    url: $post_url"
 
-    [[ "$message" =~ \{body\} ]] \
-    && msg_title="${message%\{body\}*}" \
-    && msg_body="${message#*\{body\}}" \
-
-    ([ -z "$remote_target" -o -z "$msg_title" -o -z "$msg_body" ]) \
-    && return 14 #no argval
-
-    # echo "title:$msg_title 
-    #       body:$msg_body
-    #       head:$branch
-    #       base:$repo
-    #       url:$repo/pulls"
-
-    #github API call
+    github API call
     response=`curl -v \
     -u "$user:ee693db1978cc7792f1881b3433dde25fa384f25" \
     -H "Content-Type: application/json" \
     -H "Authorization: token ee693db1978cc7792f1881b3433dde25fa384f25" \
-    -X POST -d "{\"title\":\"$msg_title\",\"head\":\"$branch\",\"base\":\"$remote_target\",\"body\":\"$msg_body\"}" \
-    "https://git.autodesk.com/api/v3/repos/$repo/pulls"`
+    -X POST -d "{\"title\":\"$msg_title\",\"head\":\"$branch\",\"base\":\"$base\",\"body\":\"$msg_body\"}" \
+    "$post_url"`
 
     _ret=$?
     [ $_ret -gt 0 ] && ret=$_ret
 
     if [ -n "$response" ]; then
         #get _links.self which has one entry - html: url
-        local self_url=($(echo `awk -v var="$response" 'c&&!--c;/.*self.*":/{c=1}'`))
-        local url=${self_url#*:}
+        local self_url=($(echo `awk -v var="$response" 'c&&!--c;/.*self.*:/{c=1}'`))
+        local url=`sed -e 's/\(https:\/\/\)\([^\/]*\)\(.*\)/\1\2\3/g' <<<$self_url`
 
-        echo "Opening url: $url"
+        echo "Opening url: $self_url"
         # open -a "/Applications/Google Chrome.app" "$url" || ret=$?
     fi
 
@@ -49,9 +53,22 @@ cmd_request(){
 }
 
 get_remote_repo_name(){
-    local repo_url=`git config --get remote.$1.url`
+    local repo_url=`get_remote_url`
     local repo_name=`dirname $repo_url | sed -e 's/.*\/\(.*\)/\1/'`
     local repo_main=`echo ${repo_url##*/} | sed -e 's/\(.*\).git/\1/'`
 
     echo "$repo_name/$repo_main"
 }
+
+get_remote_base_url(){
+    local repo_url=`get_remote_url`
+    #regex match only https://domain.com
+    echo "`sed -e 's/\(https:\/\/\)\([^\/]*\)\(.*\)/\1\2/g' <<<$repo_url`"
+}
+
+get_remote_url(){
+    local repo_label=`get_current_repo` 
+    local repo_url=`git config --get remote.$repo_label.url`   
+    echo "$repo_url" 
+}
+
