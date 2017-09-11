005603f451e18cd450bb941a4c4ada516287f4ca
proc0 <alanvx@gmail.com>
2017-09-10 22:31:27 -0700

    adding back command

--- src/cmd/back.sh
+++ src/cmd/back.sh
@@ -1,7 +1,7 @@
 cmd_back(){
 	set -x
 	local prev_branch=`kvget prev_source`
-	[ -n "$prev_branch" ] && kvset 'source' "$prev_source"
+	[ -n "$prev_branch" ] && kvset 'source' "$prev_branch"
 	cmd_jump
 	return $?
 }
